a71b6a58375c06ca2505e23d501847f17e3e303b
Ivan Serrano <contato.ivanserrano@gmail.com>
2022-05-02 09:47:55 -0500

    conceptos php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Hola, curso de php \n";
+
+
+$nombre = "Ivan";
+$apellido = "Serrano";
+
+echo "Mi nombre completo es " . $nombre . " " . $apellido . "\n";;
+echo "Mi nombre completo es $nombre $apellido \n";
+
+
+echo "el resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Ivan" => 65
+];
+
+var_dump( $personas );
+print_r( $personas );
+
+//comentarios
+# tambien es un comentario
+/*Comentarios de
+varias lineas*/ 
+echo "\n"; 
